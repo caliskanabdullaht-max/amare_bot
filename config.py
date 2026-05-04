9f4d30df581c7b1dac28a319e6c0b87f25783d9f
TURGAY KOPUK <caliskan.abdullah.t@gmail.com>
2026-05-05 01:00:52 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# API anahtarlarını buraya yaz
+META_ACCESS_TOKEN = "YOUR_META_API_TOKEN"
+LINKEDIN_EMAIL = "YOUR_EMAIL"
+LINKEDIN_PASS = "YOUR_PASSWORD"
+
+# Genel linkler
+REGISTER_LINK = "https://www.myamareglobal.com/790901/tr-TR/enrollment/register?checkccö=1"
+VIDEOS = {
+    "intro": "https://youtube.com/...introvideo",
+    "income": "https://vimeo.com/...income",
+    "product": "https://amareglobal.com/...product"
+}
