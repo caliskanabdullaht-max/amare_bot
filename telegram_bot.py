9e8078ccbdf8cfb02a3deecfa188131cc87344f3
TURGAY KOPUK <caliskan.abdullah.t@gmail.com>
2026-05-05 00:59:55 +0200

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,15 @@
+import telebot
+from message_flow import get_intro_message, get_followup_message
+from config import TELEGRAM_TOKEN
+
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    text = message.text.lower()
+    if text in ["merhaba", "selam"]:
+        bot.reply_to(message, get_intro_message())
+    else:
+        bot.reply_to(message, get_followup_message(text))
+
+bot.polling()
